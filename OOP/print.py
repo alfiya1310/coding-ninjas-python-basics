e64687540d05b3f6e80dd8533793577a5634ace1
Alfiya Khan <alfiya.khan@indexnine.com>
2022-09-23 23:51:12 +0530

    added polynomial and print name code

--- OOP/print.py
+++ OOP/print.py
@@ -1,57 +1,26 @@
-import math
+from os import *
+from sys import *
+from collections import *
+from math import *
 
-class Fraction:
+class Person:
 
-    def __init__(self,numerator,denominator):
-        self.numerator = numerator
-        self.denominator = denominator
+    #create your class here
+    def __init__(self):
+        self.name = ""
+        self.age = ""
         
-    def print(self):
-        print(str(self.numerator) + "/" + str(self.denominator))
+    def setValue(self,name,age):
+        self.name = name
+        self.age = age
         
-    
-    def simplify(self):
+    def getValue(self):
+        print("The name of the person is " + self.name + " and the age is " + age + ".")
         
-        gcd = math.gcd(self.numerator,self.denominator)
-        
-        self.numerator = self.numerator // gcd
-        self.denominator = self.denominator // gcd
-        
-    def add(self,f2):
-        
-        lcm = self.denominator * f2.denominator
-        x = lcm // self.denominator
-        y = lcm // f2.denominator
-        
-        num = x * self.numerator + (y * f2.numerator)
-        
-        self.numerator = num
-        self.denominator = lcm
-        self.simplify()
-        
-    def multiply(self,f2):
-        
-        self.numerator = self.numerator * f2.numerator
-        self.denominator = self.denominator * f2.denominator
-        self.simplify();
-
 #Driver code goes here.
-num1,den1 = map(int,input().split(' '))
-f = Fraction(num1,den1)
-q = int(input())
-
-for i in range(q):
-    typ,num2,den2 = map(int,input().split(' '))
-    
-    if(typ == 1):
-        f2 = Fraction(num2,den2)
-        f.add(f2)
-        f.print()
-        
-    elif typ == 2:
-        f2 = Fraction(num2,den2)
-        f.multiply(f2)
-        f.print()
-        
-    else:
-        pass
+ 
+P = Person()
+name = input()
+age = input()
+P.setValue(name,age)
+P.getValue()
